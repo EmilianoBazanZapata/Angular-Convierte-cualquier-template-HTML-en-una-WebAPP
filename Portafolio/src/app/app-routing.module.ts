e7ae3fd2a09e31b2fa859753d9e26ad49b269760
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-18 00:21:21 -0300

    Creando las rutas para nuestro sitio

--- Portafolio/src/app/app-routing.module.ts
+++ Portafolio/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortafolioComponent } from './Components/portafolio/portafolio.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ItemComponent } from './Components/item/item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //la primer ruta que mostrare sera la del portafolio 
+  {path:'',component:PortafolioComponent},
+  {path:'about',component:AboutComponent},
+  {path:'item',component:ItemComponent},
+  {path:'**',redirectTo:'', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
