8b0f88fb12fd97189d24e144b1b5f816e3c12736
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-18 21:19:04 -0300

    Agregando en HTML en el componente Blog

--- Portafolio/src/app/app-routing.module.ts
+++ Portafolio/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { PortafolioComponent } from './Components/portafolio/portafolio.component';
 import { AboutComponent } from './Components/about/about.component';
 import { ItemComponent } from './Components/item/item.component';
+import { BlogComponent } from './Components/blog/blog.component';
 
 const routes: Routes = [
   //la primer ruta que mostrare sera la del portafolio 
   {path:'',component:PortafolioComponent},
   {path:'about',component:AboutComponent},
+  {path:'blog',component:BlogComponent},
   {path:'item',component:ItemComponent},
   {path:'**',redirectTo:'', pathMatch:'full'}
 ];
