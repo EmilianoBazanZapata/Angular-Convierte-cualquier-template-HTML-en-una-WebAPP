a671b840a90105df1824907dea8d86c7f9f61d30
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-18 23:10:56 -0300

    Agregando el HTML del componente :Contact Us

--- Portafolio/src/app/app-routing.module.ts
+++ Portafolio/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { PortafolioComponent } from './Components/portafolio/portafolio.componen
 import { AboutComponent } from './Components/about/about.component';
 import { ItemComponent } from './Components/item/item.component';
 import { BlogComponent } from './Components/blog/blog.component';
+import { ContactUsComponent } from './Components/contact-us/contact-us.component';
 
 const routes: Routes = [
   //la primer ruta que mostrare sera la del portafolio 
   {path:'',component:PortafolioComponent},
   {path:'about',component:AboutComponent},
   {path:'blog',component:BlogComponent},
+  {path:'contact',component:ContactUsComponent},
   {path:'item',component:ItemComponent},
   {path:'**',redirectTo:'', pathMatch:'full'}
 ];
