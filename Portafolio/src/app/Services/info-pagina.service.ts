a034feab5383db318b1a87823f28f00a99a76818
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-19 01:42:41 -0300

    Crear interfaz para controlar la información de la página

--- Portafolio/src/app/Services/info-pagina.service.ts
+++ Portafolio/src/app/Services/info-pagina.service.ts
@@ -1,21 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { InfoPagina } from '../Interfaces/InfoPagina.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
 
-  info:any={};
+  info:InfoPagina={};
   cargada=false;
 
   constructor(private http:HttpClient) 
   { 
     //leer el archivo json
-  this.http.get('assets/Data/dat-pagina.json').subscribe(data => 
+  this.http.get('assets/Data/dat-pagina.json').subscribe((data:InfoPagina) => 
     {
       this.cargada=true;
       console.log(data);
+      console.log(data.titulo);
       console.log(data['twitter']);
     });
   };
